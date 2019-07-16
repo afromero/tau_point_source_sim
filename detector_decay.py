8265dc0a422db56200efefbf3c67ff30051e3018
cburch0 <claire.b.burch@gmail.com>
2019-07-16 16:57:52 -0700

    this simulation samples the tau energy and the exit probability for each neutrino

--- detector_decay.py
+++ detector_decay.py
@@ -1,82 +1,68 @@
 
 import numpy as np
+from Tau_Exit_Simulator_2 import Tau_Exit_Simulator
 
-class Area:
+
+class tau_event:
 
     # initialization
-    def __init__(self, theta_src, h, R, theta_view, N):
-        self.t_src = theta_src
-        self.h = h
-        self.R = R
-        self.th_v = theta_view
-        self.phi_src = 0
-        self.n = N
+    def __init__(self, theta_src, e_dot, phi_e, t_e, rho, E_nu):
+        self.N = len(e_dot)
+        self.theta_src = theta_src
+        self.e_dot = e_dot
+        self.phi_e = phi_e
+        self.t_e =t_e
+        self.rho = rho
+        self_e_nu = E_nu
+        self.elev = np.degrees(self.theta_src) - 90
         
-        self.cos_theta_e_hor = self.R/(self.R+self.h)
-        self.theta_e_hor = np.arccos(self.cos_theta_e_hor)
-        self.A0 = (2*np.pi*self.R**2)*(1-self.cos_theta_e_hor)
+        TES1 = './high_statistics/4.0km_ice_midCS_stdEL/LUT_1e+15_eV.npz'
+        TES2 = './high_statistics/4.0km_ice_midCS_stdEL/LUT_3e+15_eV.npz'
+        TES3 = './high_statistics/4.0km_ice_midCS_stdEL/LUT_1e+16_eV.npz'
+        TES4 = './high_statistics/4.0km_ice_midCS_stdEL/LUT_3e+16_eV.npz'
+        TES5 = './high_statistics/4.0km_ice_midCS_stdEL/LUT_1e+17_eV.npz'
+        TES6 = './high_statistics/4.0km_ice_midCS_stdEL/LUT_3e+17_eV.npz'
+        TES7 = './high_statistics/4.0km_ice_midCS_stdEL/LUT_1e+18_eV.npz'
+        TES8 = './high_statistics/4.0km_ice_midCS_stdEL/LUT_3e+18_eV.npz'
+        TES9 = './high_statistics/4.0km_ice_midCS_stdEL/LUT_1e+19_eV.npz'
+        TES10 = './high_statistics/4.0km_ice_midCS_stdEL/LUT_3e+19_eV.npz'
+        TES11 = './high_statistics/4.0km_ice_midCS_stdEL/LUT_1e+20_eV.npz'
+        TES12 = './high_statistics/4.0km_ice_midCS_stdEL/LUT_3e+20_eV.npz'
+        TES13 = './high_statistics/4.0km_ice_midCS_stdEL/LUT_1e+21_eV.npz'
+
+        TESchoices = [TES1,TES2,TES3,TES4,TES5,TES6,TES7,TES8,TES9,TES10,TES11,TES12,TES13]       
+        energychoices = [15.0,15.5,16.0,16.5,17.0,17.5,18.0,18.5,19.0,19.5,20.0,20.5,21.0]
         
-    def source_coords(self):
-        self.r_x = np.sin(self.t_src) * np.cos(self.phi_src)
-        self.r_y = np.sin(self.t_src) * np.sin(self.phi_src)
-        self.r_z = np.cos(self.t_src) 
-        return self.r_x, self.r_y, self.r_z
-        
-    def dot_prod(self):
-        self.e_x, self.e_y, self.e_z = self.earth_coords()
-        self.r_x, self.r_y, self.r_z = self.source_coords()
-        self.dot = self.e_x*self.r_x +self.r_y*self.e_y + self.e_z*self.r_z
-        return self.dot
+        # selecting the simulation with the appropriate incident tau neutrino energy
+        k = energychoices.index(E_nu)
+        self.TES = Tau_Exit_Simulator(TESchoices[k])
     
-    def earth_locs(self):
-        self.phi_e = np.random.uniform(0., 2.*np.pi, self.n)
-        self.cos_theta_e = np.random.uniform(self.cos_theta_e_hor, 1, self.n)
-        self.t_e = np.arccos(self.cos_theta_e)
-        return self.phi_e, self.t_e
+    def Exit_P(self):
+        exit_p_arr = self.TES.P_exit_th_exit(self.elev)
+        return exit_p_arr
     
-    def earth_coords(self):
-        self.phi_e, self.t_e = self.earth_locs()
-        self.e_x = np.sin(self.t_e) * np.cos(self.phi_e)
-        self.e_y = np.sin(self.t_e) * np.sin(self.phi_e)
-        self.e_z = np.cos(self.t_e) 
-        return self.e_x, self.e_y, self.e_z
-    
-    def view_angle_det(self):
-        obs_x = 0
-        obs_y = 0
-        obs_z = self.R+self.h
-        
-        point_to_obs_x = obs_x - self.R*self.e_x
-        point_to_obs_y = obs_y - self.R*self.e_y
-        point_to_obs_z = obs_z - self.R*self.e_z
-        
-        norm = np.sqrt(point_to_obs_x**2 + point_to_obs_y**2 + point_to_obs_z**2)
-        
-        point_to_obs_hat_x = point_to_obs_x / norm
-        point_to_obs_hat_y = point_to_obs_y / norm
-        point_to_obs_hat_z = point_to_obs_z / norm
-        
-        obs_dot = point_to_obs_hat_x*self.r_x+point_to_obs_hat_y*self.r_y+point_to_obs_hat_z*self.r_z
-        self.angle = np.arccos(obs_dot)
-    
-        return self.angle
-    
-    def degree_eff_area(self):
-        dot = self.dot_prod()
-        angle = self.view_angle_det()
-        self.A_deg = self.A0 * 1./float(self.n) * np.sum(dot * (angle < self.th_v) * (dot>0.) )
-        return self.A_deg
+    def E_tau(self):
+        vals =np.asarray([])
+        for i in range(self.N):
+            vals = np.append(vals,self.TES.sample_energies_th_exit(90+self.elev))
+        return vals
     
     def event_retention(self):
-        dot = self.dot_prod()
-        angle = self.view_angle_det()
-        self.ret_e_x = self.e_x *  (angle < self.th_v) * (dot>0.)
-        self.ret_e_y = self.e_y *  (angle < self.th_v) * (dot>0.)
-        self.ret_e_z = self.e_z *  (angle < self.th_v) * (dot>0.)
-        self.ret_e_dot = dot*  (angle < self.th_v) * (dot>0.)
-        self.ret_phi_e = np.arctan(self.ret_e_y / self.ret_e_x)
-        self.ret_t_e = np.arccos(self.ret_e_z)
-        return self.ret_phi_e, self.ret_t_e, self.ret_e_dot
+        self.tau_energy = self.E_tau()
+        
+        self.ret_tau_energy = self.tau_energy *  (self.tau_energy > 14.0) 
+        self.ret_tau_energy=self.ret_tau_energy[np.nonzero(self.ret_tau_energy)]
+        
+        self.ret_e_dot = self.e_dot*  (self.tau_energy > 14) 
+        self.ret_e_dot=self.ret_e_dot[np.nonzero(self.ret_e_dot)]
+        
+        self.ret_phi_e = self.phi_e*  (self.tau_energy > 14) 
+        self.ret_phi_e=self.ret_phi_e[np.nonzero(self.ret_phi_e )]
+        
+        self.ret_t_e = self.t_e*  (self.tau_energy > 14) 
+        self.ret_t_e=self.ret_t_e[np.nonzero(self.ret_t_e )]
+        
+        return self.ret_e_dot, self.ret_phi_e, self.ret_t_e, self.ret_tau_energy
 
 
 
