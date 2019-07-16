f2b2d23e6167188dfc5f75933e20bfe410c19056
cburch0 <claire.b.burch@gmail.com>
2019-07-16 16:58:38 -0700

    added list of detected event parameters, and flight path for each event

--- detectorgeometry.py
+++ detectorgeometry.py
@@ -28,6 +28,13 @@ class Area:
         self.dot = self.e_x*self.r_x +self.r_y*self.e_y + self.e_z*self.r_z
         return self.dot
     
+    def rho_nadir(self): # distance from observatory to Earth based on nadir angle
+        psi  = np.pi - np.arcsin(self.R * np.sin(self.t_src) / (self.R+self.h))
+        theta_E = np.pi - psi - self.t_src
+        rho_2 = (self.R+self.h)**2 + self.R**2 - 2 * (self.R+self.h)*self.R*np.cos(theta_E)
+        rho = np.sqrt(rho_2)
+        return rho 
+    
     def earth_locs(self):
         self.phi_e = np.random.uniform(0., 2.*np.pi, self.n)
         self.cos_theta_e = np.random.uniform(self.cos_theta_e_hor, 1, self.n)
@@ -59,7 +66,7 @@ class Area:
         
         obs_dot = point_to_obs_hat_x*self.r_x+point_to_obs_hat_y*self.r_y+point_to_obs_hat_z*self.r_z
         self.angle = np.arccos(obs_dot)
-    
+   
         return self.angle
     
     def degree_eff_area(self):
@@ -70,14 +77,26 @@ class Area:
     
     def event_retention(self):
         dot = self.dot_prod()
-        angle = self.view_angle_det()
-        self.ret_e_x = self.e_x *  (angle < self.th_v) * (dot>0.)
-        self.ret_e_y = self.e_y *  (angle < self.th_v) * (dot>0.)
-        self.ret_e_z = self.e_z *  (angle < self.th_v) * (dot>0.)
-        self.ret_e_dot = dot*  (angle < self.th_v) * (dot>0.)
+        self.angle = self.view_angle_det()
+        rho = self.rho_nadir()
+        self.ret_e_x = self.e_x *  (self.angle < self.th_v) * (dot>0.)
+        self.ret_e_x=self.ret_e_x[np.nonzero(self.ret_e_x)]
+        
+        self.ret_e_y = self.e_y *  (self.angle < self.th_v) * (dot>0.)
+        self.ret_e_y=self.ret_e_y[np.nonzero(self.ret_e_y)]
+        
+        self.ret_e_z = self.e_z *  (self.angle < self.th_v) * (dot>0.)
+        self.ret_e_z=self.ret_e_z[np.nonzero(self.ret_e_z)]
+        
+        self.ret_e_dot = dot*  (self.angle < self.th_v) * (dot>0.)
+        self.ret_e_dot=self.ret_e_dot[np.nonzero(self.ret_e_dot)]
+        
+        self.ret_rho = rho*  (self.angle < self.th_v) * (dot>0.)
+        self.ret_rho=self.ret_rho[np.nonzero(self.ret_rho)]
+        
         self.ret_phi_e = np.arctan(self.ret_e_y / self.ret_e_x)
         self.ret_t_e = np.arccos(self.ret_e_z)
-        return self.t_src, self.ret_phi_e, self.ret_t_e
-
+        
+        return self.ret_e_dot, self.ret_phi_e, self.ret_t_e, self.ret_rho
 
 
